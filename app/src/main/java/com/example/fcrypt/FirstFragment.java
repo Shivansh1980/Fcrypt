4a4bc4140f864463d0c14c5efa5ca4d1cb621e66
Shivansh1980 <49563393+Shivansh1980@users.noreply.github.com>
2021-04-21 03:54:03 +0530

    login-backend created

--- app/src/main/java/com/example/fcrypt/FirstFragment.java
+++ app/src/main/java/com/example/fcrypt/FirstFragment.java
@@ -1,15 +1,36 @@
 package com.example.fcrypt;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class FirstFragment extends Fragment {
+    private TextView email_id;
+    private TextView password;
+    private Button login_button;
+    private Button login_to_signup_button;
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth mAuth;
 
     @Override
     public View onCreateView(
@@ -17,18 +38,61 @@ public class FirstFragment extends Fragment {
             Bundle savedInstanceState
     ) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View root = inflater.inflate(R.layout.fragment_first, container, false);
+        email_id = root.findViewById(R.id.email_id);
+        password = root.findViewById(R.id.password);
+        login_button = root.findViewById(R.id.login_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Logging In");
+        progressDialog.setMessage("Please Wait Loading...");
+
+        login_to_signup_button = root.findViewById(R.id.login_to_signup_button);
+
+        return root;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        login_to_signup_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                navController.navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();
+                login(email_id.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+    public void login(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SignInIntent", "signInWithEmail:success");
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getContext(), "Login Successfully Create a new fragment and layout for navigating to it", Toast.LENGTH_LONG).show();
+                                    progressDialog.dismiss();
+                                }
+                            },2000);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("SignInIntent", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getContext(), task.getException().toString(), Toast.LENGTH_LONG).show();
+                            progressDialog.dismiss();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/fcrypt/SecondFragment.java
+++ app/src/main/java/com/example/fcrypt/SecondFragment.java
@@ -1,34 +1,190 @@
 package com.example.fcrypt;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.squareup.picasso.Picasso;
+
+import java.util.zip.Inflater;
+
+import static android.app.Activity.RESULT_OK;
+
 public class SecondFragment extends Fragment {
+
+    private TextView email_id;
+    private TextView password;
+    private ImageView profile_picture;
+    private Button select_pic;
+    private Button signup_button;
+    private ImageButton google_signin_button;
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Uri profile_picture_uri;
+    private ProgressDialog progressDialog;
+
+    //Authentications Variables
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient googleSignInClient;
+    private static int RC_SIGN_IN = 2;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+
+        progressDialog = new ProgressDialog(getContext());
+        progressDialog.setTitle("Signing In");
+        progressDialog.setMessage("Please Wait Loading...");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View root = inflater.inflate(R.layout.fragment_second, container, false);
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mAuth = FirebaseAuth.getInstance();
+        googleSignInClient = GoogleSignIn.getClient(getContext(),gso);
+
+        email_id = root.findViewById(R.id.email_id);
+        password = root.findViewById(R.id.password);
+        profile_picture = root.findViewById(R.id.pic_preview);
+        select_pic = root.findViewById(R.id.select_pic_button);
+        google_signin_button = root.findViewById(R.id.google_signin_button);
+
+        select_pic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, PICK_IMAGE_REQUEST);
+            }
+        });
+        email_id = root.findViewById(R.id.email_id);
+        password = root.findViewById(R.id.email_id);
+        signup_button = root.findViewById(R.id.signup_button);
+
+        return root;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        signup_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                progressDialog.show();
+                String email = email_id.getText().toString();
+                String pass = password.getText().toString();
+                sign_in_with_email_and_password(email, pass);
+            }
+        });
+
+        google_signin_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();
+                Intent signInIntent = googleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            profile_picture_uri = data.getData();
+            Picasso.get().load(profile_picture_uri).into(profile_picture);
+        }
+        else if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            Exception exception = task.getException();
+            Log.d("SignInActivity", "Exception From Task has been taken and now going to check if task is successful");
+            if(task.isSuccessful()) {
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    Log.d("SignInActivity", "Task is successful now we will will get the user id using GoogleSignInAccount account");
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    Log.d("SignInActivity", "firebaseAuthWithGoogle user ID : :" + account.getId());
+                    firebaseAuthWithGoogle(account.getIdToken());
+                } catch (ApiException e) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("SignInActivity", "Google sign in failed", e);
+                    // ...
+                }
+            }
+            else{
+                Log.w("SignInActivity", exception.toString());
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("SignInActivity", "signInWithCredential:success");
+
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getContext(), "You Have Been Successfully Registered", Toast.LENGTH_SHORT).show();
+                                }
+                            },1500);
+                        } else {
+                            Log.w("SignInActivity", "signInWithCredential:failure", task.getException());
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void sign_in_with_email_and_password(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                } else {
+                    Toast.makeText(getContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }
+        });
+    }
+
 }
