ee4b52f2aa6a6538a9b30668d1423cd7c646818a
Shivansh1980 <49563393+Shivansh1980@users.noreply.github.com>
2021-05-02 10:51:13 +0530

    updated

--- app/src/main/java/com/example/fcrypt/DashboardFragment.java
+++ app/src/main/java/com/example/fcrypt/DashboardFragment.java
@@ -32,19 +32,6 @@ public class DashboardFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DashboardFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static DashboardFragment newInstance(String param1, String param2) {
         DashboardFragment fragment = new DashboardFragment();
         Bundle args = new Bundle();
