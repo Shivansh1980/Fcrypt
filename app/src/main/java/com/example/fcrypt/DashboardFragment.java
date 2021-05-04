85c9a51fa27c5523f64272dc6a5c8963f39e2380
Shivansh1980 <49563393+Shivansh1980@users.noreply.github.com>
2021-05-04 09:35:15 +0530

    layout updated

--- app/src/main/java/com/example/fcrypt/DashboardFragment.java
+++ app/src/main/java/com/example/fcrypt/DashboardFragment.java
@@ -3,10 +3,13 @@ package com.example.fcrypt;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.smarteist.autoimageslider.DefaultSliderView;
@@ -55,10 +58,19 @@ public class DashboardFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_dashboard, container, false);
+        Button encryption_button = view.findViewById(R.id.file_encryption_button);
+        encryption_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavController navController = Navigation.findNavController(getActivity(),R.id.nav_host_fragment_for_nav_home);
+                navController.navigate(R.id.action_dashboardFragment_to_encryptorFragment);
+            }
+        });
         sliderLayout = view.findViewById(R.id.slider);
         sliderLayout.setIndicatorAnimation(IndicatorAnimations.FILL);
         sliderLayout.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
         sliderLayout.setScrollTimeInSec(3);
+
 
         setsliderViews();
 
